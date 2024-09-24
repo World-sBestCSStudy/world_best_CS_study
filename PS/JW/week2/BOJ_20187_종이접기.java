814812b63d4f51e5e848693b9730fdb5623023e2
jaewonwi <jww5555@naver.com>
2024-09-25 00:33:19 +0900

    week2

--- PS/JW/week2/BOJ_20187_종이접기.java
+++ PS/JW/week2/BOJ_20187_종이접기.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+// 결국 마지막에 수직, 수평으로 접은 것을 펼친 2*2의 반복이다
 public class BOJ_20187_종이접기 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
@@ -8,6 +9,8 @@ public class BOJ_20187_종이접기 {
 
     static int k, hole;
     static char[] fold;
+
+    // 수직, 수평 펼치기
     static int hor(int origin){
         switch (origin){
             case 0: return 1;
@@ -34,69 +37,49 @@ public class BOJ_20187_종이접기 {
         fold = br.readLine().toCharArray();
         hole = Integer.parseInt(br.readLine());
 
-        // 종이 크기 2^k
+        // 전체 종이 크기 2^k
         int size = (int) Math.pow(2, k);
         int[][] map = new int[size][size];
 
-        // 매칭시킬 종이
+        // 제일 작은 2*2 배열, 그걸 찾기 위한 수직, 수평 배열
         int[][] minMap = new int[2][2];
         int[] minVert = new int[2];
         int[] minHori = new int[2];
 
-
-        boolean vert = false;
+        // 수직, 수평 방향으로 한번 이상 펼쳤는지 체크하는 플래그
+        boolean vertflag = false;
         boolean horflag = false;
 
         for (int i = fold.length - 1; i >= 0; i--){
-            // 마지막 수직, 수평 접은 것 다 펼쳤으면 2*2 배열 구하기 끝
-            if (vert && horflag)
+            // 마지막 수직, 수평 접은 것 한번씩 펼쳤으면 2*2 배열 구하기 끝
+            if (vertflag && horflag)
                 break;
 
             char d = fold[i];
 
-            if (!vert){
+            if (!vertflag){
                 if (d == 'U'){  // 위로 접었다면 반대로 아래로 펴주자
-                    if (hole < 2) {
-                        minVert[0] = hole;
-                        minVert[1] = ver(hole);
-                    } else {
-                        minVert[0] = hole;
-                        minVert[1] = ver(hole);
-                    }
+                    minVert[0] = hole;
+                    minVert[1] = ver(hole);
 
-                    vert = true;
+                    vertflag = true;
                 } else if (d == 'D') {    // 아래로 접었다면 위로 펴주자
-                    if (hole < 2) {
-                        minVert[0] = ver(hole);
-                        minVert[1] = hole;
-                    } else {
-                        minVert[0] = ver(hole);
-                        minVert[1] = hole;
-                    }
+                    minVert[0] = ver(hole);
+                    minVert[1] = hole;
 
-                    vert = true;
+                    vertflag = true;
                 }
             }
 
             if (!horflag){
                 if (d == 'L'){  // 왼쪽으로 접었다면 오른쪽으로 펴주자
-                    if (hole / 2 == 0){
-                        minHori[0] = hole;
-                        minHori[1] = hor(hole);
-                    } else {
-                        minHori[0] = hole;
-                        minHori[1] = hor(hole);
-                    }
+                    minHori[0] = hole;
+                    minHori[1] = hor(hole);
 
                     horflag = true;
                 } else if (d == 'R'){   // 오른쪽으로 접었다면 왼쪽으로 펴주자
-                    if (hole / 2 == 0){
-                        minHori[0] = hor(hole);
-                        minHori[1] = hole;
-                    } else {
-                        minHori[0] = hor(hole);
-                        minHori[1] = hole;
-                    }
+                    minHori[0] = hor(hole);
+                    minHori[1] = hole;
 
                     horflag = true;
                 }
@@ -114,7 +97,7 @@ public class BOJ_20187_종이접기 {
                 rem = i;
         }
 
-        // 수직, 수평 배열을 hole을 기준으로 매칭해서 반복되는 4칸을 찾는다
+        // 수직, 수평 배열을 hole을 기준으로 매칭해서 반복되는 2*2 배열을 찾는다
         if (minVert[0] == hole && minHori[0] == hole) {   // 0번이 겹치면
             minMap[0][0] = hole;
             minMap[0][1] = minHori[1];
@@ -137,6 +120,7 @@ public class BOJ_20187_종이접기 {
             minMap[1][1] = hole;
         }
 
+        // 2*2를 전체 배열에 반복해서 넣어줌
         for (int i = 0; i < size; i += 2){
             for (int j = 0; j < size; j += 2){
                 for (int r = 0; r < 2; r++){
@@ -147,6 +131,7 @@ public class BOJ_20187_종이접기 {
             }
         }
 
+        // 출력
         for (int i = 0; i < size; i++){
             for (int j = 0; j < size; j++){
                 sb.append(map[i][j]).append(" ");
